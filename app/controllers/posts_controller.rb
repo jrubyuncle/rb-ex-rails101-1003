9768b5260218a26892a83635bd45f1071fffbad6
wesleychu <jrubyuncle@gmail.com>
2015-10-29 20:30:09 +0800

    implement actions: edit, update and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,12 +15,27 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @group = Group.find params[:group_id]
+    @post = @group.posts.find params[:id]
   end
 
   def update
+    @group = Group.find params[:group_id]
+    @post = @group.posts.find params[:id]
+
+    if @post.update post_params
+      redirect_to group_path(@group), notice: "post modified ok"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @group = Group.find params[:group_id]
+    @post = @group.posts.find params[:id]
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "post deleted"
   end
 
   private
