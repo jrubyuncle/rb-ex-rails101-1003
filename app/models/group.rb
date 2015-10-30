3b2766f3f9fa2e84be17f38e5c3eaf2ba212a946
wesleychu <jrubyuncle@gmail.com>
2015-10-30 15:18:51 +0800

    display edit/delete buttons only for group owner

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,9 @@ class Group < ActiveRecord::Base
 
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   validates :title, presence: true
+
+  def editable_by?(user)
+    user && user == owner
+  end
+
 end
